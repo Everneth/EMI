4aee5d01db50e5111ff13e80e155c98df675ab79
Mike White <mike.wildcats@gmail.com>
2018-10-24 16:21:51 -0400

    added IDB dependency. Initial config. Ready for first table to set MOTD

--- src/main/java/com/everneth/EMI/EMI.java
+++ src/main/java/com/everneth/EMI/EMI.java
@@ -1,24 +1,37 @@
 package com.everneth.EMI;
 
 import co.aikar.commands.BukkitCommandManager;
+import co.aikar.idb.DB;
+import co.aikar.idb.Database;
+import co.aikar.idb.DatabaseOptions;
+import co.aikar.idb.PooledDatabaseOptions;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EMI extends JavaPlugin {
 
     private static EMI plugin;
     private static BukkitCommandManager commandManager;
+    FileConfiguration config = getConfig();
 
 
     @Override
     public void onEnable() {
         plugin = this;
         getLogger().info("Ministry Interface started.");
+        loadConfig();
+        saveConfig();
+
+        DatabaseOptions options = DatabaseOptions.builder().mysql(config.getString("dbuser"), config.getString("dbpass"), config.getString("dbname"), config.getString("dbhost")).build();
+        Database db = PooledDatabaseOptions.builder().options(options).createHikariDatabase();
+        DB.setGlobalDatabase(db);
 
         registerCommands();
     }
     @Override
     public void onDisable() {
         getLogger().info("Ministry Interface stopped.");
+        DB.close();
     }
 
     private void registerCommands()
@@ -27,5 +40,15 @@ public class EMI extends JavaPlugin {
         commandManager.enableUnstableAPI("help");
         commandManager.getCommandContexts().registerContext();
 
+    }
+
+    private void loadConfig()
+    {
+        config.addDefault("dbhost", "localhost:3306");
+        config.addDefault("dbname", "emi");
+        config.addDefault("dbuser", "admin_emi");
+        config.addDefault("dbpass", "secret");
+        config.addDefault("dbprefix", "ev_");
+        config.options().copyDefaults(true);
     }
 }
--- src/main/java/com/everneth/EMI/commands/mint/MotdCommand.java
+++ src/main/java/com/everneth/EMI/commands/mint/MotdCommand.java
@@ -1,7 +1,18 @@
 package com.everneth.EMI.commands.mint;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.entity.Player;
 
+@CommandPermission("emi.mint.motd")
 public class MotdCommand extends BaseCommand {
     // TODO: Annotations & Calls
+
+    @Subcommand("set")
+    @CommandPermission("emi.mint.motd.set")
+    public static void onSet(Player player, String motd)
+    {
+        // TODO: Database table to query
+    }
 }
