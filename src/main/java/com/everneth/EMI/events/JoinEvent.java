b2aa101893b8037afc148f9f5f5ddc939a51cdd8
Mike White <mike.wildcats@gmail.com>
2018-10-29 11:32:05 -0400

    complete the future in constructor so that we dont need to catch exceptions

--- src/main/java/com/everneth/EMI/events/JoinEvent.java
+++ src/main/java/com/everneth/EMI/events/JoinEvent.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -20,44 +21,38 @@ public class JoinEvent implements Listener {
 
     private List<Motd> motdList;
     private CompletableFuture<List<DbRow>> futureList;
-    private DbRow[] futureArray;
+    private List<DbRow> rows;
 
     public JoinEvent()
     {
+        rows = new ArrayList<DbRow>();
+        motdList = new ArrayList<Motd>();
         futureList = DB.getResultsAsync("SELECT * FROM motds").toCompletableFuture();
-        try {
-            futureArray = futureList.get().toArray(futureArray);
-        }
-        catch(ExecutionException e)
-        {
-
-        }
-        catch(InterruptedException e)
-        {
-
-        }
-        if(futureList.isDone()) {
-            for (int i = 0; i < futureArray.length; i++) {
-                motdList.add(new Motd(
-                        futureArray[i].getInt("id"),
-                        futureArray[i].getInt("player_id"),
-                        futureArray[i].getString("message"),
-                        futureArray[i].get("isPublic")));
-
-            }
-        }
+        futureList.complete(rows);
+        buildMotdList(rows);
     }
+
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
-
-
         Player p = event.getPlayer();
         if(p.hasPermission("emi.mint.motd"))
         {
             List<Motd> result = motdList.stream().filter(motd -> Objects.equals(motd.getId(), 3)).collect(Collectors.toList());
             p.sendMessage(result.get(0).getMessage());
+        }
+    }
+
+    private void buildMotdList(List<DbRow> rows)
+    {
+        for(DbRow row : rows)
+        {
+            this.motdList.add(new Motd(
+                    row.getInt("id"),
+                    row.getInt("player_id"),
+                    row.getString("message"),
+                    row.get("isPublic")));
         }
     }
 }
