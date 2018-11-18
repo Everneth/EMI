7f6e146e3528f990dcb77d30ac802e25879fac04
Mike White <mike.wildcats@gmail.com>
2018-11-18 18:30:17 -0500

    Address #15. Mintar support added to the report embed.

--- src/main/java/com/everneth/emi/commands/ReportCommand.java
+++ src/main/java/com/everneth/emi/commands/ReportCommand.java
@@ -12,6 +12,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @CommandAlias("report")
 public class ReportCommand extends BaseCommand {
     //private JDA jda;
@@ -24,25 +27,27 @@ public class ReportCommand extends BaseCommand {
     @CatchUnknown
     public void onReport(CommandSender sender, String message)
     {
-
-
         Player player = (Player)sender;
+
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
         EmbedBuilder eb = new EmbedBuilder();
         eb.setTitle(player.getName());
         eb.setDescription(player.getUniqueId().toString());
         eb.setColor(0xffff00);
+        eb.setThumbnail("https://minotar.net/helm/" + player.getUniqueId() + "/100.png");
         eb.addField("X", Double.toString(player.getLocation().getX()), true);
         eb.addField("Y", Double.toString(player.getLocation().getY()), true);
         eb.addField("Z", Double.toString(player.getLocation().getZ()), true);
         eb.addField("Dimension", player.getWorld().getEnvironment().toString(), true);
-        eb.addField("Time", Long.toString(player.getPlayerTime()), true);
+        eb.addField("Time Reported (EST)", format.format(now), true);
+        eb.addField("Server", player.getServer().getServerName(), true);
         eb.addField("Description", message, false);
         eb.setFooter("Help requested!", null);
-
+        
         EMI.getJda().getTextChannelById(EMI.getPlugin().getConfig().getString("report-channel")).sendMessage(eb.build()).queue();
-        player.sendMessage(ChatColor.GREEN + "Report submitted to " + ChatColor.GOLD +
-                "#help " + ChatColor.GREEN + "!");
-
+        player.sendMessage(ChatColor.GREEN + "Report submitted to " + ChatColor.RED +
+                "#help" + ChatColor.GREEN + "!");
     }
 }
