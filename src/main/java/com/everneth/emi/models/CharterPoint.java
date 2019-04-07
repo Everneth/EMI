531199eaafa368fd538f95ffd7ee0dc187a6fd19
Mike <mike.wildcats@gmail.com>
2019-04-07 13:28:35 -0400

    get all active points method

--- src/main/java/com/everneth/emi/models/CharterPoint.java
+++ src/main/java/com/everneth/emi/models/CharterPoint.java
@@ -3,11 +3,16 @@ package com.everneth.emi.models;
 import co.aikar.idb.DB;
 import co.aikar.idb.DbRow;
 import com.everneth.emi.EMI;
+import com.sun.xml.internal.ws.util.CompletedFuture;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -80,11 +85,72 @@ public class CharterPoint {
         }
     }
 
+    public List<CharterPoint> getAllPoints(String name)
+    {
+        DbRow recipient = getPlayerRow(name);
+
+        List<DbRow> recordsList = new ArrayList<DbRow>();
+
+        try {
+            recordsList = DB.getResultsAsync("SELECT * FROM charter_points WHERE issued_to = ? AND date_expired = ?",
+                    recipient.getInt("player_id"),
+                    null).get();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        List<CharterPoint> pointsList = new ArrayList<CharterPoint>();
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(recipient.getString("player_uuid")));
+
+        for(DbRow record : recordsList)
+        {
+            pointsList.add(new CharterPoint(null, offlinePlayer.getPlayer(), record.getString("reason"), record.getInt("amount")));
+        }
+        return pointsList;
+    }
+
+    public DbRow getCharterPoint()
+    {
+
+    }
+
     private DbRow getPlayerRow(UUID uuid)
     {
         CompletableFuture<DbRow> futurePlayer;
         DbRow player = new DbRow();
         futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_uuid = ?", uuid.toString());
+        try {
+            player = futurePlayer.get();
+        }
+        catch (Exception e)
+        {
+            EMI.getPlugin().getLogger().info(e.getMessage());
+        }
+        return player;
+    }
+
+    private DbRow getPlayerRow(int id)
+    {
+        CompletableFuture<DbRow> futurePlayer;
+        DbRow player = new DbRow();
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_id = ?", id);
+        try {
+            player = futurePlayer.get();
+        }
+        catch (Exception e)
+        {
+            EMI.getPlugin().getLogger().info(e.getMessage());
+        }
+        return player;
+    }
+
+    private DbRow getPlayerRow(String playerName)
+    {
+        CompletableFuture<DbRow> futurePlayer;
+        DbRow player = new DbRow();
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_name = ?", playerName);
         try {
             player = futurePlayer.get();
         }
