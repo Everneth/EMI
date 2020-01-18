0f627f3a5a9778aac86eac701f745761185436f3
Mike White <mike.wildcats@gmail.com>
2020-01-17 23:40:49 -0500

    merge conflict fix

--- src/main/java/com/everneth/emi/models/Notification.java
+++ src/main/java/com/everneth/emi/models/Notification.java
@@ -12,8 +12,7 @@ public class Notification {
     private EMIPlayer recipient;
     private EMIPlayer notifier;
 
-    public Notification(EMIPlayer recipient, EMIPlayer notifier, String type, String message, Date dateTime)
-    {
+    public Notification(EMIPlayer recipient, EMIPlayer notifier, String type, String message, Date dateTime) {
         this.recipient = recipient;
         this.notifier = notifier;
         this.type = type;
@@ -21,16 +20,13 @@ public class Notification {
         this.dateTime = dateTime;
     }
 
-    public List<Notification> getAllNotifications()
-    {
+    public List<Notification> getAllNotifications() {
         List<Notification> notifications = new ArrayList<>();
         return notifications;
     }
 
-    public List<Notification> getAllNotificationsByID(int id)
-    {
+    public List<Notification> getAllNotificationsByID(int id) {
         List<Notification> notifications = new ArrayList<>();
         return notifications;
     }
-
 }
