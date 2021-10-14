ef036b8c819a97aea810bb24d7f3de4cdea84862
Jordan Wren <jordanmwren@gmail.com>
2021-10-14 13:25:19 -0600

    Update PlayerUtils to include static syncExists method

--- src/main/java/com/everneth/emi/utils/PlayerUtils.java
+++ src/main/java/com/everneth/emi/utils/PlayerUtils.java
@@ -5,7 +5,14 @@ import co.aikar.idb.DbRow;
 import com.everneth.emi.EMI;
 import com.everneth.emi.models.CharterPoint;
 import com.everneth.emi.models.EMIPlayer;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -16,7 +23,52 @@ public class PlayerUtils {
     {
         CompletableFuture<DbRow> futurePlayer;
         DbRow player = new DbRow();
-        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_name = ?", playerName);
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE ? IN (player_name,alt_name)", playerName);
+        try {
+            player = futurePlayer.get();
+        }
+        catch (Exception e)
+        {
+            EMI.getPlugin().getLogger().info(e.getMessage());
+        }
+        return player;
+    }
+
+    public static DbRow getPlayerRow(int id)
+    {
+        CompletableFuture<DbRow> futurePlayer;
+        DbRow player = new DbRow();
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_id = ?", id);
+        try {
+            player = futurePlayer.get();
+        }
+        catch (Exception e)
+        {
+            EMI.getPlugin().getLogger().info(e.getMessage());
+        }
+        return player;
+    }
+
+    public static DbRow getPlayerRow(long discordId)
+    {
+        CompletableFuture<DbRow> futurePlayer;
+        DbRow player = new DbRow();
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE discord_id = ?", discordId);
+        try {
+            player = futurePlayer.get();
+        }
+        catch (Exception e)
+        {
+            EMI.getPlugin().getLogger().info(e.getMessage());
+        }
+        return player;
+    }
+
+    public static DbRow getPlayerRow(UUID uuid)
+    {
+        CompletableFuture<DbRow> futurePlayer;
+        DbRow player = new DbRow();
+        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE ? IN (player_uuid,alt_uuid)", uuid.toString());
         try {
             player = futurePlayer.get();
         }
@@ -33,10 +85,10 @@ public class PlayerUtils {
 
         List<DbRow> recordsList = new ArrayList<DbRow>();
         try {
-                recordsList = DB.getResultsAsync("SELECT charter_point_id, p1.player_name as 'issued_to', p1.player_uuid as 'recipient_uuid', p2.player_name as 'issued_by', p2.player_uuid as 'issuer_uuid', reason, amount, date_issued, date_expired, expunged FROM charter_points c INNER JOIN\n" +
-                                "players p1 ON c.issued_to = p1.player_id\n" +
-                                "JOIN players p2 ON c.issued_by = p2.player_id WHERE issued_to = ?",
-                        recipient.getInt("player_id")).get();
+            recordsList = DB.getResultsAsync("SELECT charter_point_id, p1.player_name as 'issued_to', p1.player_uuid as 'recipient_uuid', p2.player_name as 'issued_by', p2.player_uuid as 'issuer_uuid', reason, amount, date_issued, date_expired, expunged FROM charter_points c INNER JOIN\n" +
+                            "players p1 ON c.issued_to = p1.player_id\n" +
+                            "JOIN players p2 ON c.issued_by = p2.player_id WHERE issued_to = ?",
+                    recipient.getInt("player_id")).get();
         }
         catch (Exception e)
         {
@@ -84,35 +136,6 @@ public class PlayerUtils {
             );
         }
     }
-    public static DbRow getPlayerRow(int id)
-    {
-        CompletableFuture<DbRow> futurePlayer;
-        DbRow player = new DbRow();
-        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_id = ?", id);
-        try {
-            player = futurePlayer.get();
-        }
-        catch (Exception e)
-        {
-            EMI.getPlugin().getLogger().info(e.getMessage());
-        }
-        return player;
-    }
-
-    public static DbRow getPlayerRow(long discordId)
-    {
-        CompletableFuture<DbRow> futurePlayer;
-        DbRow player = new DbRow();
-        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE discord_id = ?", discordId);
-        try {
-            player = futurePlayer.get();
-        }
-        catch (Exception e)
-        {
-            EMI.getPlugin().getLogger().info(e.getMessage());
-        }
-        return player;
-    }
 
     public static DbRow getAppRecord(long discordId)
     {
@@ -127,6 +150,15 @@ public class PlayerUtils {
             EMI.getPlugin().getLogger().info(e.getMessage());
         }
         return player;
+    }
+
+    public static boolean syncExists(UUID uuid) {
+        DbRow playerRow = getPlayerRow(uuid);
+        Long discordId = 0L;
+        if (playerRow != null)
+            discordId = playerRow.getLong("discord_id");
+
+        return discordId != 0;
     }
 
     public static boolean isMemberAlready(long discordId)
@@ -147,21 +179,6 @@ public class PlayerUtils {
             return true;
     }
 
-    public static DbRow getPlayerRow(UUID uuid)
-    {
-        CompletableFuture<DbRow> futurePlayer;
-        DbRow player = new DbRow();
-        futurePlayer = DB.getFirstRowAsync("SELECT * FROM players WHERE player_uuid = ?", uuid.toString());
-        try {
-            player = futurePlayer.get();
-        }
-        catch (Exception e)
-        {
-            EMI.getPlugin().getLogger().info(e.getMessage());
-        }
-        return player;
-    }
-
     public static EMIPlayer getEMIPlayer(String name)
     {
         DbRow playerRow = PlayerUtils.getPlayerRow(name);
@@ -171,5 +188,34 @@ public class PlayerUtils {
     {
         DbRow playerRow = PlayerUtils.getPlayerRow(uuid);
         return new EMIPlayer(playerRow.getString("player_uuid"), playerRow.getString("player_name"), playerRow.getInt("player_id"));
+    }
+
+    public static UUID getPlayerUUID(String name) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.mojang.com/users/profiles/minecraft/" + name);
+        StringBuffer sb = null;
+        try {
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() != 204) {
+                JSONObject obj = new JSONObject(EntityUtils.toString(response.getEntity()));
+
+                sb = new StringBuffer(obj.getString("id"));
+                sb.insert(8, "-");
+
+                sb = new StringBuffer(sb.toString());
+                sb.insert(13, "-");
+
+                sb = new StringBuffer(sb.toString());
+                sb.insert(18, "-");
+
+                sb = new StringBuffer(sb.toString());
+                sb.insert(23, "-");
+
+            } else
+                return null;
+        } catch (IOException e) {
+            EMI.getPlugin().getLogger().warning(e.getMessage());
+        }
+        return UUID.fromString(sb.toString());
     }
 }
