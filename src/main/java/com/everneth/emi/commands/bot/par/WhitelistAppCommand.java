128ed8646d5de10b40d18f887933533f8fbb8f4a
jmwren <jordanmwren@gmail.com>
2021-12-29 20:14:53 -0700

    Added preconditions for whitelist app commands

--- src/main/java/com/everneth/emi/commands/bot/par/WhitelistAppCommand.java
+++ src/main/java/com/everneth/emi/commands/bot/par/WhitelistAppCommand.java
@@ -22,6 +22,8 @@ public class WhitelistAppCommand extends SlashCommand {
         this.help = "All commands pertaining to user applications";
 
         this.defaultEnabled = false;
+        this.enabledRoles = new String[]{EMI.getPlugin().getConfig().getString("staff-role-id")};
+        
         this.children = new SlashCommand[]{new GetAllApps(), new GetApp()};
     }
 
