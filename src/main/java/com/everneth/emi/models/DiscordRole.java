00acf1d625807850ddd3b1dfc4b1426b091280fd
jmwren <jordanmwren@gmail.com>
2022-07-05 20:45:23 -0600

    Created DiscordRole enum

---
+++ src/main/java/com/everneth/emi/models/DiscordRole.java
@@ -0,0 +1,23 @@
+package com.everneth.emi.models;
+
+import com.everneth.emi.EMI;
+import net.dv8tion.jda.api.entities.Role;
+
+public enum DiscordRole {
+    CITIZEN("member-role-id"),
+    MINT("mint-role-id"),
+    STAFF("staff-role-id"),
+    APPLICANT("applicant-role-id"),
+    PENDING("pending-role-id"),
+    SYNCED("synced-role-id");
+
+    private final String keyValue;
+
+    private DiscordRole(String keyValue) {
+        this.keyValue = keyValue;
+    }
+
+    public Role get() {
+        return EMI.getGuild().getRoleById(EMI.getConfigLong(keyValue));
+    }
+}
